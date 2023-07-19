d8728097b0a71ae5f6a181eeb7a6ca8ca8179496
CHYA <40337979+cyang0513@users.noreply.github.com>
2023-07-19 09:37:31 +0200

    Update Program.cs

--- src/TestConsole/Program.cs
+++ src/TestConsole/Program.cs
@@ -1,41 +1,37 @@
-﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Data;
 
 namespace TestConsole
 {
-   internal class Program
-   {
-      static void Main(string[] args)
-      {
-         Console.WriteLine("Test started...");
-         var cover = new SQLServerCoverage.CodeCoverage("Data Source=.;Initial Catalog=TestDb;Integrated Security=True;TrustServerCertificate=True", "TestDb");
-         cover.Start(30000);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test started...");
+            var cover = new SQLServerCoverage.CodeCoverage("Data Source=.;Initial Catalog=MRE_RO_DEV;Integrated Security=True;TrustServerCertificate=True", "MRE_RO_DEV");
+            cover.Start(30000);
 
-         using (var sqlCon = new SqlConnection("Data Source=.;Initial Catalog=TestDb;Integrated Security=True;TrustServerCertificate=True"))
-         {
-            sqlCon.Open();
-            SqlCommand sql_cmnd = new SqlCommand("p_Cal", sqlCon);
-            sql_cmnd.CommandType = CommandType.StoredProcedure;
-            sql_cmnd.Parameters.AddWithValue("@v1", SqlDbType.Int).Value = 5;
-            sql_cmnd.Parameters.AddWithValue("@v2", SqlDbType.Int).Value = 10;
-            var returnVal = sql_cmnd.Parameters.Add("@v3", SqlDbType.Int);
-            returnVal.Direction = ParameterDirection.ReturnValue;
-            sql_cmnd.ExecuteNonQuery();
+            using (var sqlCon = new SqlConnection("Data Source=.;Initial Catalog=MRE_RO_DEV;Integrated Security=True;TrustServerCertificate=True"))
+            {
+                sqlCon.Open();
+                SqlCommand sql_cmnd = new SqlCommand("[TestHelpers].[p_TruncateDerivativeData]", sqlCon);
+                sql_cmnd.CommandType = CommandType.StoredProcedure;
+                sql_cmnd.ExecuteNonQuery();
 
-            Console.WriteLine($"Return {returnVal.Value}");
+                Console.WriteLine($"Done!");
 
-            sqlCon.Close();
-         }
+                sqlCon.Close();
+            }
 
-         var result = cover.Stop();
+            var result = cover.Stop();
 
-         result.SaveSourceFiles(".");
-         result.ToOpenCoverXml("opencover.xml");
-         result.ToHtml("coverage.html", ".", "opencover.xml");
+            result.SaveSourceFiles(".\\SqlSource", true);
+            result.ToOpenCoverXml("opencover.xml");
+            result.ToHtml("coverage.html", ".\\SqlSource", "opencover.xml");
 
-         Console.WriteLine("Test done...");
+            Console.WriteLine("Test done...");
 
-         Console.ReadKey();
-      }
-   }
+            Console.ReadKey();
+        }
+    }
 }
